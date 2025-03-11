e226e51f7b3a8222b189b91d1f1ef5f0dca86d25
juancrfig <juancrfig@gmail.com>
2025-03-12 00:35:53 +0100

    Finish as much queries as I could

--- dql_select.sql
+++ dql_select.sql
@@ -97,3 +97,100 @@ GROUP BY cat.nombre;
 
 -- CONSULTA 8
 -- Calcula el promedio de días que un cliente tarda en devolver las películas alquiladas
+SELECT AVG(TIMESTAMPDIFF(DAY, fecha_alquiler, fecha_devolucion)) AS dias_diferencia FROM alquiler;
+
+
+
+-- Consulta 9
+-- Encuentra los cinco empleados que gestionaron más alquileres en la categoría de Acción.
+
+SELECT e.nombre, COUNT(*) AS numero_alquileres_gestionados
+FROM pago AS p
+JOIN alquiler AS a ON a.id_alquiler = p.id_alquiler
+JOIN inventario AS i ON i.id_inventario = a.id_inventario
+JOIN pelicula AS peli ON peli.id_pelicula = i.id_pelicula
+JOIN pelicula_categoria AS pc ON pc.id_pelicula = peli.id_pelicula
+JOIN categoria AS c ON c.id_categoria = pc.id_categoria
+JOIN empleado AS e ON e.id_empleado = p.id_empleado
+WHERE c.nombre = 'Action'
+GROUP BY p.id_empleado;
+
+
+-- Consulta 10
+-- Genera un informe de los clientes con alquileres más recurrentes.
+
+
+
+-- Consulta 11
+-- Calcula el costo promedio de alquiler por idioma de las películas.
+
+SELECT idi.nombre, AVG(p.total) AS costo_promedio 
+FROM pelicula AS peli
+JOIN idioma AS idi ON idi.id_idioma = peli.id_idioma
+JOIN inventario AS inv ON inv.id_pelicula = peli.id_pelicula
+JOIN alquiler AS a ON a.id_inventario = inv.id_inventario
+JOIN pago AS p ON p.id_alquiler = a.id_alquiler
+GROUP BY idi.nombre;
+
+-- La consulta retorna solo un resultado porque en la base de datos solo hay registros
+-- para el idioma ingles
+
+
+
+-- Consulta 12
+-- Lista las cinco películas con mayor duración alquiladas en el último año.
+
+SELECT * FROM alquiler ORDER BY fecha_alquiler DESC LIMIT 1;
+-- Esto nos muestra que en los registros el ultimo año es 2019.
+-- Por lo tanto vamos a usar este año para realizar la consulta principal
+
+SELECT peli.titulo
+FROM pelicula AS peli
+JOIN inventario AS inv ON inv.id_pelicula = peli.id_pelicula
+JOIN alquiler AS a ON a.id_inventario = inv.id_inventario
+WHERE YEAR(a.fecha_alquiler) = '2019'
+ORDER BY peli.duracion DESC
+LIMIT 5;
+
+-- Nuevamente el resultado solo es una pelicula
+
+
+-- CONSULTA 14
+-- Encuentra la cantidad total de días alquilados por cada cliente en el último mes.
+
+SELECT * FROM alquiler ORDER BY fecha_alquiler DESC LIMIT 1;
+-- Esta consulta nos muestra que el ultimo mes de registros es Enero del año 2019
+-- Usaremos esto para la consulta principal
+
+
+-- CONSULTA 17
+-- Encuentra el cliente que ha realizado el alquiler más caro en el último año.
+
+SELECT * FROM alquiler ORDER BY fecha_alquiler DESC LIMIT 1;
+-- Esta consulta nos muestra que el ultimo año es 2019
+-- Usaremos esto para la consulta principal
+
+SELECT c.nombre, c.apellidos 
+FROM cliente AS c
+JOIN alquiler AS a ON a.id_cliente = c.id_cliente
+JOIN pago AS p ON p.id_cliente = c.id_cliente
+WHERE YEAR(a.fecha_alquiler) = '2019'
+ORDER BY p.total DESC
+LIMIT 1;
+
+
+
+-- CONSULTA 20
+-- Lista los clientes que no han realizado ningún alquiler en el último año.
+
+SELECT * FROM alquiler ORDER BY fecha_alquiler DESC LIMIT 1;
+-- Esta consulta nos muestra que el ultimo año es 2019
+-- Usaremos esto para la consulta principal
+
+SELECT c.nombre, c.apellidos 
+FROM cliente AS c
+WHERE c.id_cliente NOT IN (
+    SELECT id_cliente 
+    FROM alquiler
+    WHERE YEAR(fecha_alquiler) = '2019'
+);
