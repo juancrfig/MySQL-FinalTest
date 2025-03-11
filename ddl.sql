54157eb0477a136334bef5ada624238465a1372d
juancrfig <juancrfig@gmail.com>
2025-03-11 21:13:44 +0100

    Create table categoria

--- ddl.sql
+++ ddl.sql
@@ -1,24 +1,7 @@
 -- Following good practices for database creation statements
 DROP DATABASE IF EXISTS sakilacampus;
-CREATE DATABASE sakila CHARACTER SET utf8mb4;
+CREATE DATABASE sakilacampus CHARACTER SET utf8mb4;
 USE sakilacampus;
-
-
--- Create table "pago"
-
-CREATE TABLE (
-    id_pago SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_cliente SMALLINT UNSIGNED NOT NULL,
-    id_empleado TINYINT UNSIGNED NOT NULL,
-    id_alquiler INT UNSIGNED NOT NULL,
-    total DECIMAL(5,2) NOT NULL,
-    fecha_pago DATETIME NOT NULL,
-    ultima_actualizacion TIMESTAMP NOT NULL
-    FOREIGN KEY()
-);
-
-
-
 
 
 -- Create table "actor"
@@ -32,10 +15,41 @@ CREATE TABLE actor (
 
 -- Create table "idioma"
 
-CREATE TABLE (
+CREATE TABLE idioma (
     id_idioma TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(20) NOT NULL CHECK(TRIM(nombre) != ''),
     ultima_actualizacion TIMESTAMP NOT NULL
+);
+
+-- Create table "pais"
+
+CREATE TABLE pais (
+    id_pais SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL CHECK(TRIM(nombre) != ''),
+    ultima_actualizacion TIMESTAMP NOT NULL
+);
+
+-- Create table "ciudad"
+
+CREATE TABLE ciudad (
+    id_ciudad SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL CHECK(TRIM(nombre) != ''),
+    id_pais SMALLINT UNSIGNED NOT NULL,
+    FOREIGN KEY(id_pais) REFERENCES pais(id_pais)
+);
+
+-- Create table direccion
+
+CREATE TABLE direccion (
+    id_direccion SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    direccion VARCHAR(50) NOT NULL CHECK(TRIM(direccion) != ''),
+    direccion2 VARCHAR(50) NOT NULL CHECK(TRIM(direccion2) != ''),
+    distrito VARCHAR(20) NOT NULL CHECK(TRIM(distrito) != ''),
+    id_ciudad SMALLINT UNSIGNED NOT NULL,
+    codigo_postal VARCHAR(10) NOT NULL CHECK(TRIM(codigo_postal) != ''),
+    telefono VARCHAR(20) NOT NULL CHECK(TRIM(telefono) != ''),
+    ultima_actualizacion TIMESTAMP NOT NULL,
+    FOREIGN KEY(id_ciudad) REFERENCES ciudad(id_ciudad)
 );
 
 -- Create table "empleado"
@@ -55,37 +69,6 @@ CREATE TABLE empleado (
     FOREIGN KEY(id_direccion) REFERENCES direccion(id_direccion)
 );
 
--- Create table direccion
-
-CREATE TABLE direccion (
-    id_direccion SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    direccion VARCHAR(50) NOT NULL CHECK(TRIM(direccion) != ''),
-    direccion2 VARCHAR(50) NOT NULL CHECK(TRIM(direccion2) != ''),
-    distrito VARCHAR(20) NOT NULL CHECK(TRIM(distrito) != ''),
-    id_ciudad SMALLINT UNSIGNED NOT NULL,
-    codigo_postal VARCHAR(10) NOT NULL CHECK(TRIM(codigo_postal) != ''),
-    telefono VARCHAR(20) NOT NULL CHECK(TRIM(telefono) != ''),
-    ultima_actualizacion TIMESTAMP NOT NULL,
-    FOREIGN KEY(id_ciudad) REFERENCES ciudad(id_ciudad)
-);
-
--- Create table "ciudad"
-
-CREATE TABLE ciudad (
-    id_ciudad SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL CHECK(TRIM(nombre) != ''),
-    id_pais SMALLINT UNSIGNED NOT NULL,
-    FOREIGN KEY(id_pais) REFERENCES pais(id_pais)
-);
-
--- Create table "pais"
-
-CREATE TABLE pais (
-    id_pais SMALLINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL CHECK(TRIM(nombre) != ''),
-    ultima_actualizacion TIMESTAMP NOT NULL
-);
-
 -- Create table "film_text"
 
 CREATE TABLE film_text (
@@ -93,3 +76,12 @@ CREATE TABLE film_text (
     title VARCHAR(255) NOT NULL CHECK(TRIM(title) != ''),
     description TEXT NOT NULL
 );
+
+-- Create table categoria
+
+CREATE TABLE categoria (
+    id_categoria TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(25) NOT NULL CHECK(TRIM(nombre) != ''),
+    ultima_actualizacion TIMESTAMP NOT NULL
+);
+
